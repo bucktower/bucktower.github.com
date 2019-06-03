8d43a37c9609f24d4acf8e36d67beae7041ae9ae
Buck Tower <buck@bucktower.net>
2019-06-02 20:04:15 -0500

    Random bobbles

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,6 @@
+// Randomize the speed at which boxes move
+var boxes = document.getElementsByClassName("box");
 
+for (var i = 0; i < boxes.length; i++) {
+  boxes[i].style.setProperty('--animation-time', (Math.random() + 1) * 2 + 's');
+}
